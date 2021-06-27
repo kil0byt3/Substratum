76ea3b653f29fab8c09c3ec390fb13f0c98d07ea
kil0byt3 <alex.b.gust@gmail.com>
2021-06-27 13:46:20 -0700

    FINAL BUILD: Substratum 1.0.0 Server Pack (poggers)

---
+++ _SERVER PACK/Substratum Server 1.0.0/ServerStart.bat
@@ -0,0 +1,20 @@
+@echo off
+rem Substratum Server Startup Script
+
+rem Edit the below values to change JVM Arguments or Allocated RAM for the server.
+set ALLOCATED_RAM=4G
+set JVM_ARGUMENTS=-XX:+UseG1GC -XX:+UnlockExperimentalVMOptions -XX:MaxGCPauseMillis=100 -XX:+DisableExplicitGC -XX:TargetSurvivorRatio=90 -XX:G1NewSizePercent=50 -XX:G1MaxNewSizePercent=80 -XX:G1MixedGCLiveThresholdPercent=50 -XX:+AlwaysPreTouch
+
+rem Make sure this matches the Forge version of the server if you update.
+set FORGE_VERSION=forge-1.16.5-36.1.30
+
+rem Install Minecraft Forge if it isn't already installed:
+if not exist .\%FORGE_VERSION%.jar (
+    echo Installing Minecraft Forge Server
+    java -jar %FORGE_VERSION%-installer.jar --installServer
+)
+
+echo Starting Substratum Server
+java -jar -Xms%ALLOCATED_RAM% -Xmx%ALLOCATED_RAM% %JVM_ARGUMENTS% %FORGE_VERSION%.jar nogui
+
+pause
---
+++ _SERVER PACK/Substratum Server 1.0.0/scripts/jei_descriptions.zs
@@ -0,0 +1,8 @@
+import crafttweaker.api.item.IItemStack;
+import crafttweaker.api.blocks.MCBlock;
+import crafttweaker.api.tag.MCTag;
+import crafttweaker.api.BlastFurnaceManager;
+import crafttweaker.api.registries.IRecipeManager;
+
+mods.jei.JEI.addInfo(<item:waystones:waystone>, ["Waystones are disabled, and are only be meant to be placed by server's admins in points of interest!", "In the early game, try using Carts, Boats, and Roads for travel!", "Path blocks give you a speed boost!", "Later game, check JEI for the Warp Plate block for two-way teleporters!"]);
+mods.jei.JEI.addInfo(<item:enigmaticlegacy:recall_potion>, ["Brew an Eye of Ender into an Awkward Potion to create Potions of Recall."]);
